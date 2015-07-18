7db3bb891e6a9964b19b3da268fe1eee0ef219a8
Hrishi Olickel <hrishioa@gmail.com>
2015-07-18 03:49:13 +0000

    Raspberry Pi Setup files

---
+++ Raspi-Code/Lib/self_test.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+
+def Blink(numTimes, speed):
+	for i in range(0, numTimes):
+		print "Iteration "+str(i+1)
+		GPIO.output(26,True)
+		time.sleep(speed)
+		GPIO.output(26,False)
+		time.sleep(speed)
+	print "Done"
+	GPIO.cleanup()
+
+iterations = raw_input("Enter number of times to blink: ")
+speed = raw_input("Length of each blink in seconds: ")
+
+Blink(int(iterations),float(speed))
