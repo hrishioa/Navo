73354db1548a42ac931cb227fe5206ff7d0e0434
Hrishi Olickel <hrishioa@gmail.com>
2015-07-23 19:40:28 +0000

    Added Relay Test Code

---
+++ Raspi-Code/relay.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(22,GPIO.IN)
+
+for i in range(0,10):
+        GPIO.output(4, True)
+        GPIO.output(24,True)
+	GPIO.output(25,False)
+	GPIO.output(23,False)
+	time.sleep(0.25)
+	GPIO.output(4, False)
+	GPIO.output(24,False)
+        GPIO.output(25,True)
+        GPIO.output(23,True)
+	time.sleep(0.25)
+
+previr=0
+
+while True:
+	newir=GPIO.input(22)
+	if(newir!= previr):
+		print "IR Changed to %d." % (newir)
+		previr=newir
+
+
+GPIO.cleanup()
